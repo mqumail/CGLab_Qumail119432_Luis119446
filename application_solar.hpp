8add69adcddd56aa0aebf5032bfc97c5f38206ea
TheMichaelWilde <33606490+TheMichaelWilde@users.noreply.github.com>
2018-10-29 23:16:22 +0100

    Add files via upload

---
+++ application_solar.hpp
@@ -0,0 +1,55 @@
+#ifndef APPLICATION_SOLAR_HPP
+#define APPLICATION_SOLAR_HPP
+
+#include "application.hpp"
+#include "model.hpp"
+#include "structs.hpp"
+#include "SceneGraph.hpp"
+
+
+// gpu representation of model
+class ApplicationSolar : public Application {
+ public:
+  // allocate and initialize objects
+  ApplicationSolar(std::string const& resource_path);
+  // free allocated objects
+  ~ApplicationSolar();
+
+  // react to key input
+  void keyCallback(int key, int action, int mods);
+  //handle delta mouse movement input
+  void mouseCallback(double pos_x, double pos_y);
+  //handle resizing
+  void resizeCallback(unsigned width, unsigned height);
+
+  //Hack to get this stupid framework to work
+  void outputPlanet(float x, float y, float z);
+
+
+  // draw all objects
+  void render();
+
+ protected:
+  void initializeShaderPrograms();
+  void initializeGeometry();
+  // update uniform values
+  void uploadUniforms();
+  // upload projection matrix
+  void uploadProjection();
+  // upload view matrix
+  void uploadView();
+
+  // cpu representation of model
+  model_object planet_object;
+  
+  // camera transform matrix
+  glm::fmat4 m_view_transform;
+  // camera projection matrix
+  glm::fmat4 m_view_projection;
+
+  // theSceneGraph
+  SceneGraph theSceneGraph;
+
+};
+
+#endif
